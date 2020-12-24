d312a07b7da796afe05d5c0fb1784095f7efc85f
ashaytiwari <ashaytiwari2001@gmail.com>
2020-12-23 20:47:57 -0800

    feat: Body Parsing and handling cors error by headers

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,14 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity,
+        orderBy: req.body.orderBy
+    };
     res.status(201).json({
-        message: 'Order Created'
+        message: 'Order Created',
+        orderCreated: order
     });
 });
 
--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'It works !'
-//     });
-// });
+// Headers for handling cors error provided by Browser
+app.use((req, res, next) => {
+    res.header("Allow-Control-Allow-Origin", "*");
+    res.header("Allow-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+    if(req.method === 'OPTIONS'){
+        res.header("Allow-Control-Allow-Methods", "PUT, PATCH, DELETE, POST, GET");
+        return res.status(200).json({});
+    }
+    next();
+})
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
