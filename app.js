e70e5ad443e68defd37311709e32ef8f777570e7
ashaytiwari <ashaytiwari2001@gmail.com>
2020-12-24 16:15:42 -0800

    feat: Mongo db and mongoose setup, first version of /products api is ready

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Connecting the Database
+mongoose.connect("mongodb+srv://ashay:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-restful-shop-api.u1dn8.mongodb.net/" +
+    process.env.MONGO_ATLAS_DB +
+    "?retryWrites=true&w=majority", { 
+        useNewUrlParser: true 
+})
+
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Headers for handling cors error provided by Browser
 app.use((req, res, next) => {
     res.header("Allow-Control-Allow-Origin", "*");
     res.header("Allow-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
-    if(req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.header("Allow-Control-Allow-Methods", "PUT, PATCH, DELETE, POST, GET");
         return res.status(200).json({});
     }
